149dbfad8654ba2fa63c8dacb6b54a79d61d0d1e
Francis Lewis <francis.lewis@me.com>
2019-04-11 17:57:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,4 @@ function destructivelyRemoveFirstKitten() {
 const appendKitten = name => [...kittens, name]
 const prependKitten = name => [name, ...kittens]
 const removeLastKitten = () => kittens.slice(0, kittens.length - 1)
+const removeFirstKitten = () => kittens.slice(1)
